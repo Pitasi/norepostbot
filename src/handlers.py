4de25ab75db89938629c7853e13972b30f0e3a7e
Antonio Pitasi <pitasi.antonio@gmail.com>
2018-08-20 19:45:48 +0200

    Increase treshold for similar images

--- src/handlers.py
+++ src/handlers.py
@@ -40,7 +40,7 @@ def hash_photo(update):
             Image.open(
                 io.BytesIO(photo.get_file().download_as_bytearray())
             ),
-            hash_size=8
+            hash_size=16
         ))
         if not h in res:
             res.append(h)
